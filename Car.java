78beeaa44e5dabed787edf82d5b57627a72832a7
Viktoria <viktoria.magnusson9@gmail.com>
2021-11-04 14:07:48 +0100

    Moved variable declarations and extended subtypes

--- Car.java
+++ Car.java
@@ -1,2 +1,13 @@
-public class Car {
+import java.awt.*;
+
+public abstract class Car {
+    public int nrDoors; // Number of doors on the car
+    public double enginePower; // Engine power of the car
+    public double currentSpeed; // The current speed of the car
+    public Color color; // Color of the car
+    public String modelName; // The car model name
+
+    public Car(){
+
+    }
 }
