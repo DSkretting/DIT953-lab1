562c3f1cf8994968f6c963880a37c44257478d3e
dpmram <supersnigeln48@gmail.com>
2021-11-12 11:39:05 +0100

    added tests for saab speedfactor and turbo

--- Test/Lab1Test.java
+++ Test/Lab1Test.java
@@ -1,7 +1,23 @@
+import Lab1.Saab95;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Lab1Test {
+    @Test
+    void saab_speedfactor_without_turbo() {
+        Saab95 set = new Saab95();
+        set.setTurboOff();
+        set.speedFactor();
+        assertEquals(1.25, set.speedFactor());
+    }
+    @Test
+    void saab_speedfactor_with_turbo() {
+        Saab95 set = new Saab95();
+        set.setTurboOn();
+        set.speedFactor();
+        assertEquals(1.625, set.speedFactor());
+
+    }
 }
 
