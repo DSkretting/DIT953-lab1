cacd89d8fe42be221265eabbe9c06ddaed51df14
DSkretting <91507477+DSkretting@users.noreply.github.com>
2021-11-12 11:39:31 +0100

    Added Tests

--- Test/Lab1Test.java
+++ Test/Lab1Test.java
@@ -1,7 +1,20 @@
+import Lab1.Volvo240;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class Lab1Test {
+ class Lab1Test {
+
+     @Test
+     void volvoSpeedFactor()   {
+         Volvo240 set = new Volvo240();
+         set.speedFactor();
+         assertEquals(1.25, set.speedFactor());
+
+     }
 }
+
+
+
 
