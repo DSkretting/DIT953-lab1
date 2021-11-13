dea8aee5467cb7754bc33b57b6accf836df1690f
DSkretting <91507477+DSkretting@users.noreply.github.com>
2021-11-13 18:53:14 +0100

    Separated Volvo240 and Saab95 into 2 different Test classes added @BeforeAll and did some more tests.

--- Test/Lab1Test.java
+++ Test/Lab1Test.java
@@ -1,6 +1,7 @@
 import Lab1.Main;
 import Lab1.Saab95;
 import Lab1.Volvo240;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -9,56 +10,74 @@ import java.awt.event.KeyEvent;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class Lab1Test {
+class SaabTest {
     private static Saab95 saab95;
-
-    @Test
-    void saab_speedfactor_without_turbo() {
-        Saab95 set = new Saab95();
-        set.setTurboOff();
-        set.speedFactor();
-        assertEquals(1.25, set.speedFactor());
-    }
-    @Test
-    void saab_speedfactor_with_turbo() {
-        Saab95 set = new Saab95();
-        set.setTurboOn();
-        set.speedFactor();
-        assertEquals(1.625, set.speedFactor());
-
-    }
-
-     @Test
-     void volvoSpeedFactor()   {
-         Volvo240 set = new Volvo240();
-         set.speedFactor();
-         assertEquals(1.25, set.speedFactor());
-
-     }
-     @Test
-     void SaabDoors() {
+    @BeforeAll
+    static void setSaab95() {
         saab95 = new Saab95();
+    }
+    @Test
+    void Saab95SpeedFactorWithoutTurbo() {
+        saab95.setTurboOff();
+        assertEquals(1.25, saab95.speedFactor());
+    }
+    @Test
+    void Saab95SpeedFactorWithTurbo() {
+        saab95.setTurboOn();
+        assertEquals(1.625, saab95.speedFactor());
+    }
+     @Test
+     void Saab95Doors() {
         assertEquals(2, saab95.getNrDoors());
     }
     @Test
-     void SaabColor() {
-        saab95 = new Saab95();
+     void Saab95Color() {
         assertEquals(Color.red, saab95.getColor());
     }
     @Test
-    void SaabEnginePower() {
-        saab95 = new Saab95();
+    void Saab95EnginePower() {
         assertEquals(125, saab95.getEnginePower());
     }
 //    @Test
 //    void SaabName() {
-//        saab95 = new Saab95();
 //        assertEquals("Saab95", saab95.getModelName());
 //    }
     @Test
-    void SaabCurrentSpeed() {
-        saab95 = new Saab95();
+    void Saab95CurrentSpeed() {
         assertEquals(1, saab95.getCurrentSpeed());
     }
 }
+class Volvo240Test{
+    private static Volvo240 volvo240;
+    @BeforeAll
+    static void setVolvo240() {
+        volvo240 = new Volvo240();
+    }
+    @Test
+    void Volvo240SpeedFactor() {
+        assertEquals(1.25, volvo240.speedFactor());
+    }
+    @Test
+    void Volvo240Doors() {
+        assertEquals(4, volvo240.getNrDoors());
+    }
+    @Test
+    void Volvo240Color() {
+        assertEquals(Color.black, volvo240.getColor());
+    }
+    @Test
+    void Volvo240EnginePower() {
+        assertEquals(100, volvo240.getEnginePower());
+    }
+//    @Test
+//    void Volvo240Name() {
+//        assertEquals("Volvo240", volvo240.getModelName());
+//    }
+    @Test
+    void Volvo240CurrentSpeed() {
+        assertEquals(1, volvo240.getCurrentSpeed());
+        // CurrentSpeed för Volvo är skum den är 1 i konstruktorn men det ger värdet 1 när man testar den
+    }
+}
 
+// Vet inte vaför getModelName inte funkar.
